acba5073c6f484ae4f91c662c9c5ff49021e22aa
Abhiraj <btech10735.21@bitmesra.ac.in>
2024-12-08 22:51:45 +0530

    updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
